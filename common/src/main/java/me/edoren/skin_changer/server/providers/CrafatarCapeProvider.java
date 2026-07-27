b7c7209731a9c62ea6bb9c006831b7a264994df1
shradinx <luphcore@gmail.com>
2026-07-27 15:00:50 -0400

    replace crafatar.com with crafatar-pub.neodium.fr due to former going offline

--- common/src/main/java/me/edoren/skin_changer/server/providers/CrafatarCapeProvider.java
+++ common/src/main/java/me/edoren/skin_changer/server/providers/CrafatarCapeProvider.java
@@ -7,6 +7,6 @@ public class CrafatarCapeProvider implements ISkinProvider {
     public byte[] getSkin(String playerName) {
         String uuid = NetworkUtils.getPlayerUUID(playerName);
         if (uuid == null) return null;
-        return NetworkUtils.downloadFile(String.format("https://crafatar.com/capes/%s", uuid), null, 2);
+        return NetworkUtils.downloadFile(String.format("https://crafatar-pub.neodium.fr/capes/%s", uuid), null, 2);
     }
 }
